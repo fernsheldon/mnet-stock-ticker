94c2a6d6e4f758c771e956e4aa623e01cbd37421
Sheldon Fernandes <fernsheldon@gmail.com>
2017-08-13 19:39:27 +0530

    Creating the app.js file for serving the data

--- app/app.js
+++ app/app.js
@@ -1,13 +1,6 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-
-
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
-
 angular.module('stockTicker', [
     'ngRoute',
     'stockTicker.view1',
